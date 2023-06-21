78b9ab470db13da5e2e000f3d7800a1c9a4b85e2
recursive-beast <splentercell.1997@gmail.com>
2023-06-21 16:24:35 +0100

    accept auth token from cookie

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -2,14 +2,25 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+function fromTokenCookie(req: Request) {
+  var token = req.cookies.token;
+  if (typeof token === 'string')
+	return token;
+  return null;
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        fromTokenCookie,
+      ]),
       ignoreExpiration: false,
-      secretOrKey: configService.get("JWT_SECRET"),
+      secretOrKey: configService.get('JWT_SECRET'),
     });
   }
 
