894949e526e41acc7f0e70b52f3e04af413ad7d8
recursive-beast <splentercell.1997@gmail.com>
2023-06-25 17:57:06 +0100

    replace custom PrismaModule with nestjs-prisma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const prismaService = app.get(PrismaService);
+  const { httpAdapter } = app.get(HttpAdapterHost);
   const cookie_secret = configService.get('COOKIE_SECRET');
 
-  app.use(cookieParser(cookie_secret));
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(cookieParser(cookie_secret));
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class UserService {
@@ -18,13 +18,6 @@ export class UserService {
       where: { id },
     });
 
-    return user;
-  }
-
-  async findByIdOrThrow(id: number) {
-    const user = await this.findById(id);
-
-    if (!user) throw new NotFoundException();
     return user;
   }
 
