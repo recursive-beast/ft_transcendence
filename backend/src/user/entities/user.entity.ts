24cfc98a35dec662db36ebebb210ed6ae322f4ce
recursive-beast <splentercell.1997@gmail.com>
2023-07-22 21:43:27 +0100

    backend: improve UserEntity type safety

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { Exclude, Expose, Type, plainToInstance } from 'class-transformer';
 import { ClassTransformerGroups } from 'src/common/enum';
 
 export class UserEntity implements User {
+  @Expose()
   id: number;
 
   @Exclude()
@@ -11,8 +12,13 @@ export class UserEntity implements User {
   @Exclude()
   googleId: string | null;
 
+  @Expose()
   username: string;
+
+  @Expose()
   fullname: string;
+
+  @Expose()
   image: string | null;
 
   @Exclude()
@@ -33,10 +39,18 @@ export class UserEntity implements User {
   @Type(() => UserEntity)
   blockedBy?: UserEntity[];
 
+  @Expose()
   createdAt: Date;
+
+  @Expose()
   updatedAt: Date;
 
-  static fromUser(user: User | User[]) {
-    return plainToInstance(UserEntity, user, { ignoreDecorators: true });
+  static fromUser(user: User): UserEntity;
+  static fromUser(user: User[]): UserEntity[];
+  static fromUser(user: User | User[]): UserEntity | UserEntity[] {
+    return plainToInstance(UserEntity, user, {
+      ignoreDecorators: true,
+      excludeExtraneousValues: true,
+    });
   }
 }
