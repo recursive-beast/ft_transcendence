688d5250dae93a6a8d969d08bd41975aae83fbc7
Mohammed Messaoudi <messaoudi1337@gmail.com>
2023-10-03 22:14:13 +0100

    landing page >> finish description & features sections

--- frontend/src/app/landing/page.js
+++ frontend/src/app/landing/page.js
@@ -12,19 +12,19 @@ export default function Home() {
   return (
     <main className="bg-bg01 ">
       {/* Herro section */}
-      <header className="w-screen h-screen flex items-stretch">
+      <header className="w-screen h-screen flex items-stretch mb-36">
         {/* background image */}
-      <Image
-        alt="Mountains"
-        src={heroImg}
-        placeholder="blur"
-        quality={100}
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: "cover",
-        }}
-      />
+        <Image
+          alt="Mountains"
+          src={heroImg}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+          }}
+        />
         {/* left side logo and start button */}
         <div className="flex flex-col justify-between my-20 ml-20 sticky">
           <Image
@@ -63,15 +63,59 @@ export default function Home() {
         <div className="flex -rotate-90 mb-32 mt-auto space-x-2">
           <button
             onClick={() => setOn(!on)}
-            className=" text-tx02 text-sm font-extralight tracking-[3px] uppercase"
+            className=" text-tx02 text-sm font-light tracking-[3px] uppercase"
           >
             sound
           </button>
-          <div className=" text-tx01 text-sm font-extralight tracking-[3px] uppercase w-10">
+          <div className=" text-tx01 text-sm font-light tracking-[3px] uppercase w-10">
             {on ? "on" : "off"}
           </div>
         </div>
       </header>
+
+      {/* description section */}
+      <section className="mb-36">
+        <div className="w-3/4 mx-auto">
+          <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase mb-8">
+            Paddle Smash
+          </div>
+
+          <p className="text-tx02 text-3xl font-medium tracking-[3px] uppercase">
+            Experience the timeless joy of <span className="text-tx06">ping pong</span> Enjoy a
+            straightforward gameplay that captures the essence of the classic
+            game while providing hours of entertainment
+          </p>
+        </div>
+      </section>
+      
+      {/* Features section */}
+      <section>
+        <div className="w-3/4 mx-auto">
+          <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase pb-8 border-b border-tx02">
+          Paddle Smash
+          </div>
+
+          <div className="text-tx02 text-3xl font-semibold tracking-[3px] uppercase py-6 border-b">
+          Accelerating <br />gameplay
+          </div>
+
+          <div className="text-tx02 text-3xl font-semibold tracking-[3px] uppercase py-6 border-b">
+          Classic <br />graphics
+          </div>
+
+          <div className="text-tx02 text-3xl font-semibold tracking-[3px] uppercase py-6 border-b">
+          Simple <br />controls
+          </div>
+
+          <div className="text-tx02 text-3xl font-semibold tracking-[3px] uppercase py-6 border-b">
+          Multiplayer
+          </div>
+
+          <div className="text-tx02 text-3xl font-semibold tracking-[3px] uppercase py-6 border-b">
+          Realistic <br />physics
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
