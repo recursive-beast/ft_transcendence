3f286bd4c008677865056ba96ce1bfaed4c353cd
Mohammed Messaoudi <messaoudi1337@gmail.com>
2023-10-04 17:13:34 +0100

    landing page >> add technology and team sections

--- frontend/src/app/landing/page.js
+++ frontend/src/app/landing/page.js
@@ -1,10 +1,17 @@
 "use client";
 
 import Image from "next/image";
-import logoPic from "@/images/logos/logo.png";
 import { Icon } from "@iconify/react";
 import { useState } from "react";
+
 import heroImg from "@/images/pics/hero-bg.jpg";
+import footerImg from "@/images/pics/footer.jpg";
+
+// logos
+import logoPic from "@/images/logos/logo.png";
+import intrat from "@/images/logos/42_logo.png";
+import git from "@/images/logos/github.png";
+import linkedin from "@/images/logos/linkedin.png";
 
 // backend logos
 import dckCmp from "@/images/technologies/backend/docker-compose.png";
@@ -22,12 +29,17 @@ import javascript from "@/images/technologies/frontend/javascript.png";
 import nextjs from "@/images/technologies/frontend/nextjs.png";
 import react from "@/images/technologies/frontend/react.png";
 import tailwindcss from "@/images/technologies/frontend/tailwindcss.png";
+// team picturs
+import aaitoma from "@/images/profile_images/aait-oma.jpeg";
+import melhous from "@/images/profile_images/mel-hous.jpeg";
+import mmessaou from "@/images/profile_images/mmessaou.jpeg";
+import syakoubi from "@/images/profile_images/syakoubi.jpeg";
 
 export default function Home() {
   const [on, setOn] = useState(true);
 
   return (
-    <main className="bg-bg01 ">
+    <main className="bg-bg01 pb-36">
       {/* Hero section */}
       <header className="h-screen flex items-stretch mb-36">
         {/* background image */}
@@ -42,27 +54,8 @@ export default function Home() {
             objectFit: "cover",
           }}
         />
-        {/* left side logo and start button */}
-        <div className="flex flex-col justify-between my-20 ml-20 sticky">
-          <Image
-            src={logoPic}
-            alt="Logo of the game"
-            width={100}
-            height={100}
-          />
-          <button className="group flex items-center ml-6">
-            <Icon
-              className="text-tx01 group-hover:text-tx02 transition duration-500 mr-2"
-              icon="solar:gamepad-broken"
-              width="36"
-            />
-            <div className=" text-tx01 font-extralight tracking-[4.80px] uppercase opacity-0 group-hover:opacity-100 transition duration-700">
-              start
-            </div>
-          </button>
-        </div>
 
-        {/* middle "the text" */}
+        {/*"the text" */}
         <div className="space-y-14 my-auto text-center mx-auto z-10">
           <div className=" text-tx01 text-xl font-extralight tracking-[4.80px] uppercase">
             Paddle Smash
@@ -75,9 +68,33 @@ export default function Home() {
             <div className="text-tx06">balls</div>
           </div>
         </div>
+      </header>
 
+      {/* sticky elements */}
+      <section className="h-screen fixed flex items-stretch justify-between mb-36 z-10">
+        {/* left side logo and start button */}
+        <div className="flex justify-between fixed h-full top-0 left-0">
+          <div className="flex flex-col justify-between my-20 ml-16">
+            <Image
+              src={logoPic}
+              alt="Logo of the game"
+              width={100}
+              height={100}
+            />
+            <button className="group flex items-center ml-6">
+              <Icon
+                className="text-tx01 group-hover:text-tx02 transition duration-500 mr-2"
+                icon="solar:gamepad-broken"
+                width="36"
+              />
+              <div className=" text-tx01 font-extralight tracking-[4.80px] uppercase opacity-0 group-hover:opacity-100 transition duration-700">
+                start
+              </div>
+            </button>
+          </div>
+        </div>
         {/* right side sound button */}
-        <div className="flex -rotate-90 mb-32 mt-auto space-x-2">
+        <div className="flex -rotate-90 mb-32 mt-auto space-x-2 mr-6 fixed right-0 bottom-0">
           <button
             onClick={() => setOn(!on)}
             className=" text-tx02 text-sm font-light tracking-[3px] uppercase"
@@ -88,7 +105,7 @@ export default function Home() {
             {on ? "on" : "off"}
           </div>
         </div>
-      </header>
+      </section>
 
       {/* description section */}
       <section className="mb-36">
@@ -161,7 +178,7 @@ export default function Home() {
                 className="aspect-square object-contain"
                 src={v}
                 alt="Logo"
-                width={50}
+                width={60}
               />
             );
           })}
@@ -187,7 +204,7 @@ export default function Home() {
                 className="aspect-square object-contain"
                 src={v}
                 alt="Logo"
-                width={50}
+                width={60}
               />
             );
           })}
@@ -195,7 +212,219 @@ export default function Home() {
       </section>
 
       {/* team section */}
-      <section className="mb-36"></section>
+      <section className="mb-36">
+        <div className="w-3/4 mx-auto flex flex-col mb-36">
+          <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase pb-8 border-b border-tx02">
+            team
+          </div>
+
+          {/* team info */}
+          <div className="flex flex-col relative">
+            <div className="pb-24 border-b border-tx02">
+              <div className="text-tx02 text-5xl font-medium tracking-[3px] uppercase mt-16 mb-8 leading-normal">
+                mohammed <br />
+                <span className="ml-40">messaoudi</span>
+              </div>
+
+              <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase mb-8">
+                ui/ux & Frontend
+              </div>
+
+              <div className="space-x-10">
+                <button>
+                  <Image
+                    src={intrat}
+                    alt="intrat logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+
+                <button>
+                  <Image src={git} alt="git hub logo" width={40} height={40} />
+                </button>
+
+                <button>
+                  <Image
+                    src={linkedin}
+                    alt="Linkedin logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </div>
+            </div>
+
+            <div className="pb-24 border-b border-tx02">
+              <div className="text-tx02 text-5xl font-medium tracking-[3px] uppercase mt-16 mb-8 leading-normal">
+                soufian <br />
+                <span className="ml-40">yakoubi</span>
+              </div>
+
+              <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase mb-8">
+                backend & team manager
+              </div>
+
+              <div className="space-x-10">
+                <button>
+                  <Image
+                    src={intrat}
+                    alt="intrat logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+
+                <button>
+                  <Image src={git} alt="git hub logo" width={40} height={40} />
+                </button>
+
+                <button>
+                  <Image
+                    src={linkedin}
+                    alt="Linkedin logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </div>
+            </div>
+
+            <div className="pb-24 border-b border-tx02">
+              <div className="text-tx02 text-5xl font-medium tracking-[3px] uppercase mt-16 mb-8 leading-normal">
+                Mohammed Badr <br />
+                <span className="ml-40">Eddine El Housni</span>
+              </div>
+
+              <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase mb-8">
+                full stack
+              </div>
+
+              <div className="space-x-10">
+                <button>
+                  <Image
+                    src={intrat}
+                    alt="intrat logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+
+                <button>
+                  <Image src={git} alt="git hub logo" width={40} height={40} />
+                </button>
+
+                <button>
+                  <Image
+                    src={linkedin}
+                    alt="Linkedin logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </div>
+            </div>
+
+            <div className="pb-24 border-b border-tx02">
+              <div className="text-tx02 text-5xl font-medium tracking-[3px] uppercase mt-16 mb-8 leading-normal">
+                Abdeljalil <br />
+                <span className="ml-40">Ait Omar</span>
+              </div>
+
+              <div className="text-tx01 text-lg font-light tracking-[4.80px] uppercase mb-8">
+                full stack
+              </div>
+
+              <div className="space-x-10">
+                <button>
+                  <Image
+                    src={intrat}
+                    alt="intrat logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+
+                <button>
+                  <Image src={git} alt="git hub logo" width={40} height={40} />
+                </button>
+
+                <button>
+                  <Image
+                    src={linkedin}
+                    alt="Linkedin logo"
+                    width={40}
+                    height={40}
+                  />
+                </button>
+              </div>
+            </div>
+
+            {/* profile pics */}
+            <div className="w-fit h-full absolute right-10">
+              <div className="w-fit py-10 sticky top-1/3 bottom-0">
+                <ul className="space-y-5 flex flex-col items-center">
+                  <li>
+                    <Image
+                      className="rounded-full opacity-100"
+                      src={mmessaou}
+                      alt="Logo of the game"
+                      width={100}
+                      quality={100}
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      className="rounded-full opacity-70"
+                      src={syakoubi}
+                      alt="Logo of the game"
+                      width={70}
+                      quality={100}
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      className="rounded-full opacity-70"
+                      src={melhous}
+                      alt="Logo of the game"
+                      width={70}
+                      quality={100}
+                    />
+                  </li>
+                  <li>
+                    <Image
+                      className="rounded-full opacity-70"
+                      src={aaitoma}
+                      alt="Logo of the game"
+                      width={70}
+                      quality={100}
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* footer section */}
+      <section className="h-screen mb-36 relative">
+        <Image
+          alt="Mountains"
+          src={footerImg}
+          placeholder="blur"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+          }}
+        />
+
+        <div className=" text-tx01 font-bold tracking-tighter text-9xl uppercase -space-y-2 w-3/4 mx-auto text-center">
+          chi l3ibatika b7al haka fiha chi klmat
+        </div>
+      </section>
     </main>
   );
 }
