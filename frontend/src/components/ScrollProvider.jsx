3408e694b5700165a5e7f6f8bde16180817c7857
recursive-beast <splentercell.1997@gmail.com>
2023-10-26 15:13:15 +0100

    enable scroll on route change

--- frontend/src/components/ScrollProvider.jsx
+++ frontend/src/components/ScrollProvider.jsx
@@ -1,11 +1,19 @@
 "use client";
 
-const { createContext, useState } = require("react");
+import { usePathname } from "next/navigation";
+
+const { createContext, useState, useEffect } = require("react");
 
 export const ScrollContext = createContext(null);
 
-export function ScrollProvider({defaultValue, ...props}) {
+export function ScrollProvider({ defaultValue, ...props }) {
   const state = useState(defaultValue);
+  const [scroll, setScroll] = state;
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setScroll(true);
+  }, [pathname]);
 
   return <ScrollContext.Provider value={state} {...props} />;
 }
