1bcd844d10e80332233938843ab3567003990272
Mohammed Messaoudi <messaoudi1337@gmail.com>
2023-11-23 18:19:13 +0100

    fixed chat redy to add conversation to mobile

--- frontend/src/app/chat/page.js
+++ frontend/src/app/chat/page.js
@@ -4,9 +4,10 @@ import clsx from "clsx";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Bg from "@/images/pics/chatbg.png";
 import { Icon } from "@iconify/react";
 import { useEffect, useState } from "react";
+
+import logoPic from "@/images/logos/logo.png";
 //Profils
 import Pic01 from "@/images/profils/01.jpg";
 import Pic02 from "@/images/profils/02.jpg";
@@ -29,7 +30,7 @@ import {
   subDays,
 } from "date-fns";
 
-faker.seed(4);
+faker.seed(2);
 
 const conversations = Array(50)
   .fill()
@@ -71,16 +72,16 @@ function GroupMessage(props) {
         return (
           <div
             key={index}
-            className="flex cursor-pointer items-center justify-between p-2"
+            className="flex cursor-pointer border-b  border-tx03 p-2"
             onClick={() => {
               props.onConversation?.(group);
             }}
           >
             {/* flex div contain avatar, name and lastMessage */}
-            <div className="flex items-center">
+            <div className="flex flex-1 items-center">
               {/* avatar */}
               <Image
-                className="mr-2 h-12 w-12 rounded-full border-[1.5px] border-tx02 object-cover p-[2px] xs:mr-3 xs:h-14 xs:w-14"
+                className="mr-2 h-12 w-12 flex-none rounded-full border-[1.5px] border-tx02 object-cover p-[2px] xs:mr-3 xs:h-14 xs:w-14"
                 // src={group.avatar}
                 src={Pic03}
                 quality={100}
@@ -89,14 +90,14 @@ function GroupMessage(props) {
               />
 
               {/* Group name and lastMessage time */}
-              <div className="flex flex-col overflow-hidden">
+              <div className="flex flex-grow flex-col w-10">
                 {/* name */}
-                <div className=" truncate text-base tracking-widest">
+                <div className="truncate text-sm tracking-wide xs:text-base xs:tracking-widest">
                   {group.displayName}
                 </div>
 
                 {/* lastMessage */}
-                <div className="truncate text-sm text-tx02">
+                <div className="truncate text-xs xs:text-sm text-tx02">
                   {formatDistanceToNowStrict(group.messages.at(-1).date, {
                     addSuffix: true,
                   })}
@@ -105,7 +106,7 @@ function GroupMessage(props) {
             </div>
 
             {/* div contain time end nbrMessages */}
-            <div className="flex items-center justify-center -space-x-4">
+            <div className="flex items-center justify-center -space-x-3 xs:-space-x-4 ">
               <div>
                 <Image
                   src={Pic05}
@@ -147,14 +148,13 @@ function DirectMessage(props) {
         return (
           <div
             key={index}
-            className="flex cursor-pointer items-center justify-between p-2 "
+            className="flex cursor-pointer border-b border-tx03 p-2"
             onClick={() => {
               props.onConversation?.(conversation);
-              console.log(conversation);
             }}
           >
             {/* flex div contain avatar, name and lastMessage */}
-            <div className="flex flex-initial items-center overflow-hidden">
+            <div className="flex flex-1 items-center">
               {/* avatar */}
               <Image
                 className={clsx(
@@ -168,37 +168,39 @@ function DirectMessage(props) {
                 height={56}
               />
 
-              {/* name and lastMessage */}
-              <div className="flex flex-col overflow-hidden">
-                {/* name */}
-                <div className=" truncate text-base tracking-widest">
-                  {conversation.displayName}
+              <div className="flex flex-grow flex-col pr-1">
+                {/* name and time */}
+                <div className="flex items-center">
+                  {/* name */}
+                  <div className="w-10 flex-grow truncate text-sm tracking-wide xs:text-base xs:tracking-widest">
+                    {conversation.displayName}
+                  </div>
+
+                  {/* time */}
+                  <div className="text-sm text-tx02">
+                    {isToday(conversation.messages.at(-1).date)
+                      ? format(conversation.messages.at(-1).date, "HH:mm")
+                      : isYesterday(conversation.messages.at(-1).date)
+                      ? "yesterday"
+                      : format(conversation.messages.at(-1).date, "yyyy/MM/dd")}
+                  </div>
                 </div>
 
-                {/* lastMessage */}
-                <div className="truncate text-sm text-tx02">
-                  {conversation.messages.at(-1).text}
+                {/* div contain last Message end nbrMessages */}
+                <div className="flex items-center">
+                  {/* lastMessage */}
+                  <div className="w-10 flex-grow truncate text-xs xs:text-sm text-tx02">
+                    {conversation.messages.at(-1).text}
+                  </div>
+
+                  {/* nbrMessages */}
+                  {conversation.unseen != 0 && (
+                    <div className="ml-1 h-5 w-5 rounded-full bg-tx02 text-center text-sm font-semibold text-tx04">
+                      {conversation.unseen}
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-
-            {/* div contain time end nbrMessages */}
-            <div className="flex flex-col items-end">
-              {/* time */}
-              <div className="text-sm text-tx02">
-                {isToday(conversation.messages.at(-1).date)
-                  ? format(conversation.messages.at(-1).date, "HH:mm")
-                  : isYesterday(conversation.messages.at(-1).date)
-                  ? "yesterday"
-                  : format(conversation.messages.at(-1).date, "yyyy/MM/dd")}
-              </div>
-
-              {/* nbrMessages */}
-              {conversation.unseen != 0 && (
-                <div className="h-5 w-5 rounded-full bg-tx02 text-center text-sm font-semibold text-tx04">
-                  {conversation.unseen}
-                </div>
-              )}
             </div>
           </div>
         );
@@ -211,14 +213,14 @@ function BoxMessages({ onClick, ...props }) {
   return (
     <div
       className={clsx(
-        "w-full max-w-[25rem] p-1 sm:px-2",
-        props.activ && "h-[49%] shrink grow",
+        props.activ && "h-[49%] shrink grow border-b border-tx02",
         !props.activ && "h-fit",
       )}
     >
-      <div className="no-scrollbar h-full w-full overflow-auto rounded-3xl border-y border-tx01">
+      <div className="no-scrollbar h-full overflow-auto">
         <button
-          className="sticky top-0 flex w-full items-center justify-between border-b border-tx03 bg-bg01 px-2 py-4 text-base capitalize tracking-[3px] text-tx02"
+          className="sticky top-0 flex h-14 w-full items-center justify-between border-b border-tx01 bg-bg03
+            px-2 py-4 text-base capitalize tracking-[3px] text-tx02 sm:h-16"
           onClick={onClick}
         >
           <div>{props.title}</div>
@@ -234,66 +236,88 @@ function BoxMessages({ onClick, ...props }) {
 function ConversationBox(props) {
   return (
     <div className="no-scrollbar hidden h-full flex-1 flex-col justify-between overflow-auto sm:flex sm:bg-bg02">
-      {/* top bar, friend info */}
-      <button className="sticky top-0 flex w-full items-center border-b bg-bg03 py-2">
-        {/* avatar */}
-        <Image
-          className="mx-4 my-1 h-10 w-10 rounded-full border-2 border-tx02 object-cover p-[2px] xs:h-12 xs:w-12 sm:h-16 sm:w-16 xl:h-20 xl:w-20"
-          src={Pic15}
-          quality={100}
-        />
+      {props.conversation ? (
+        <div>
+          {/* top bar, friend info */}
+          <button className="sticky top-0 flex h-16 w-full items-center border-b bg-bg03 py-2">
+            {/* avatar */}
+            <Image
+              className={clsx(
+                "mx-4 my-1 h-[52px] w-[52px] rounded-full object-cover p-[2px]",
+                props.conversation && status[props.conversation.status].border,
+              )}
+              src={Pic15}
+              quality={100}
+            />
 
-        <div className="flex flex-col items-start">
-          {/* full name */}
-          <div className="text-base font-semibold capitalize tracking-[widest] text-tx05 xs:text-lg sm:text-xl sm:tracking-[3px]">
-            megashoot
+            <div className="flex flex-col items-start">
+              {/* full name */}
+              <div className="text-base font-semibold capitalize tracking-[widest] text-tx05 sm:tracking-[3px]">
+                {props.conversation && props.conversation.displayName}
+              </div>
+
+              <div className="text-[10px] font-light capitalize text-tx02 md:text-[14px]">
+                Click here to visit profile
+              </div>
+            </div>
+          </button>
+
+          {/* conversation */}
+          <div>
+            {props.conversation &&
+              props.conversation.messages.map((message) => {
+                return (
+                  <div
+                    className={clsx(
+                      "my-3 flex w-fit max-w-[70%] flex-col rounded-lg px-2 py-[2px] 2xl:py-1",
+                      message.sent
+                        ? "ml-auto mr-5 rounded-tr-none bg-tx03"
+                        : "ml-5 mr-auto rounded-tl-none bg-tx02",
+                    )}
+                  >
+                    <span>{message.text}</span>
+                    <span
+                      className={clsx(
+                        "ml-auto text-xs",
+                        message.sent ? "text-tx02" : "text-tx03",
+                      )}
+                    >
+                      {isToday(message.date)
+                        ? format(message.date, "HH:mm")
+                        : isYesterday(message.date)
+                        ? "yesterday " + format(message.date, "HH:mm")
+                        : format(message.date, "yyyy/MM/dd HH:mm")}
+                    </span>
+                  </div>
+                );
+              })}
           </div>
 
-          <div className="text-[12px] font-light capitalize text-tx02 md:text-[14px]">
-            Click here to visit profile
+          {/* input message */}
+          <div className="sticky bottom-0 flex w-full  items-center space-x-3 bg-bg03 px-5 py-3">
+            <input className=" h-8 flex-1 rounded-xl bg-tx02 text-base font-extralight xs:text-xl" />
+
+            <button>
+              <Icon
+                className=" h-7 w-7 text-tx01"
+                icon="fluent:send-32-filled"
+              />
+            </button>
           </div>
         </div>
-      </button>
+      ) : (
+        // no conversation selected
+        <div className="flex h-full flex-col items-center justify-center gap-6">
+          <Image src={logoPic} alt="Logo of the game" className="h-52 w-52" />
 
-      {/* conversation */}
-      <div>
-        {props.conversation &&
-          props.conversation.messages.map((message) => {
-            return (
-              <div
-                className={clsx(
-                  "my-3 flex w-fit max-w-[70%] flex-col rounded-lg px-2 py-[2px] 2xl:py-1",
-                  message.sent
-                    ? "ml-auto mr-5 rounded-tr-none bg-tx03"
-                    : "ml-5 mr-auto rounded-tl-none bg-tx02",
-                )}
-              >
-                <span>{message.text}</span>
-                <span
-                  className={clsx(
-                    "ml-auto text-xs",
-                    message.sent ? "text-tx02" : "text-tx03",
-                  )}
-                >
-                  {isToday(message.date)
-                    ? format(message.date, "HH:mm")
-                    : isYesterday(message.date)
-                    ? "yesterday " + format(message.date, "HH:mm")
-                    : format(message.date, "yyyy/MM/dd HH:mm")}
-                </span>
-              </div>
-            );
-          })}
-      </div>
+          <div className="text-3xl font-extralight">Select Conversation</div>
 
-      {/* input message */}
-      <div className="sticky bottom-0 flex w-full  items-center space-x-3 bg-bg03 px-5 py-3">
-        <input className=" h-8 flex-1 rounded-xl bg-tx02 text-base font-extralight xs:text-xl" />
-
-        <button>
-          <Icon className=" h-7 w-7 text-tx01" icon="fluent:send-32-filled" />
-        </button>
-      </div>
+          <div className="w-1/2 text-center text-sm text-tx02">
+            No conversation selected. Please choose a conversation to start
+            chatting.
+          </div>
+        </div>
+      )}
     </div>
   );
 }
@@ -320,9 +344,12 @@ export default function Home() {
           </div>
 
           {/* content */}
-          <div className="flex h-96 w-full flex-1 justify-center bg-bg01 p-3 sm:justify-between sm:p-5">
+          <div className="flex h-96 flex-1 bg-bg01 p-3 sm:p-5">
             {/* Messages */}
-            <div className="flex h-full w-full max-w-md flex-col items-center justify-between space-y-2 overflow-hidden border-tx03 bg-bg02 sm:w-2/5 sm:border-r">
+            <div
+              className="flex h-full flex-grow flex-col space-y-2 border-tx03 bg-bg02 
+             sm:w-[45%] sm:max-w-[25rem] sm:flex-none sm:border-r md:w-1/2"
+            >
               {/* Direct Msgs */}
               <BoxMessages
                 onClick={() => {
