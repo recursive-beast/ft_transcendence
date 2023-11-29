a85178bb7fd9956785202a66056bb421b5903615
recursive-beast <splentercell.1997@gmail.com>
2023-11-29 20:35:50 +0100

    redirect to '/' on protected routes

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -5,7 +5,7 @@ import { redirect } from "./common";
 export async function middleware(request) {
   const cookie = request.cookies.get("authorization");
 
-  if (!cookie) return redirect(request, "/auth/login");
+  if (!cookie) return redirect(request, "/");
 
   const res = await fetch(`${process.env.BACKEND_URL}/users/me`, {
     headers: {
@@ -13,7 +13,7 @@ export async function middleware(request) {
     },
   });
 
-  if (!res.ok) return redirect(request, "/auth/login");
+  if (!res.ok) return redirect(request, "/");
 
   return NextResponse.next();
 }
