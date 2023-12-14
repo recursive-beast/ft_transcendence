78cbc81a4630b5cd6a83616b26ef0163b8ef8752
recursive-beast <splentercell.1997@gmail.com>
2023-12-14 10:23:16 +0100

    drop user update validation

--- backend/src/user/dto/update.dto.ts
+++ backend/src/user/dto/update.dto.ts
@@ -1,35 +1,16 @@
-import {
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-  NotContains,
-} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UserUpdateDTO {
   @IsOptional()
   @IsNotEmpty()
   @IsString()
-  @Matches(/^[a-zA-Z][a-zA-Z0-9_-]*$/, {
-    message:
-      'displayName must start with a letter and only contain letters, digits, underscores and hyphens',
-  })
-  @MinLength(4)
-  @MaxLength(30)
-  @NotContains('admin')
-  @NotContains('root')
+  @Transform(({ value }) => value.trim().replace(/ +/g, ' ') || undefined)
   displayName?: string;
 
   @IsOptional()
   @IsNotEmpty()
   @IsString()
-  @Matches(/^[a-zA-Z]+([',. -][a-zA-Z]+)*$/, {
-    message:
-      'fullName must contain only alphabetical characters separated by spaces and common punctuation',
-  })
-  @NotContains('admin')
-  @NotContains('root')
+  @Transform(({ value }) => value.trim().replace(/ +/g, ' ') || undefined)
   fullName?: string;
 }
