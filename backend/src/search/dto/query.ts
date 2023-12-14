9bdb70b6d924adf66b3f83f138c52c4af18646ca
recursive-beast <splentercell.1997@gmail.com>
2023-12-14 17:09:38 +0100

    accept empty string as search string

--- backend/src/search/dto/query.ts
+++ backend/src/search/dto/query.ts
@@ -1,8 +1,7 @@
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class QueryDto {
-  @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value.trim())
   search: string;
